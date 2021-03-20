15b06a73c3e22708800fa823655da42eb6eaf1be
Mcl <michael707wilson@gmail.com>
2021-03-20 19:56:00 -0400

    Initial Commit

---
+++ Pong/PONG_KIT.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TysonTK;
+using System.Drawing;
+using OpenTK;
+using OpenTK.Input;
+
+namespace Pong
+{
+    public static class PONG_KIT
+    {
+        public static Color
+            STANDARD_COLOR = Color.White,
+            APERTURE_BLUE = Color.FromArgb(0, 129, 196),
+            APERTURE_ORANGE = Color.FromArgb(238, 127, 27),
+            APERTURE_PURPLE = Color.FromArgb(36, 1, 121),
+            APERTURE_CYAN = Color.FromArgb(104, 204, 255),
+            APERTURE_YELLOW = Color.FromArgb(240, 206, 89),
+            APERTURE_RED = Color.FromArgb(138, 24, 24);
+
+        public const double WIN_W = 1920, WIN_H = 1080;
+
+        public static double trig_f(double c, Ball b)
+        {
+            double m = (b.yVel / b.xVel);
+            return m * c + (b.y - m * b.x);
+        }
+    }
+
+    public class Ball
+    {
+        public double x, y, size, xVel, yVel, speed, screenW, screenH;
+        Color hue;
+
+        public Ball(double _x, double _y, double _scale, double _s, Color _c, double _screenW, double _screenH, double _xv, double _yv)
+        {
+            x = _x;
+            y = _y;
+            speed = _s;
+            hue = _c;
+            size = _scale;
+            xVel = _xv;
+            yVel = _yv;
+            screenW = _screenW;
+            screenH = _screenH;
+        }
+
+        public void run(List<Paddle> list_paddles)
+        {
+            x += xVel;
+            y += yVel;
+
+            if (y + size > screenH || y - size < -screenH) bounceY();
+            if (x + size > screenW || x - size < -screenW) bounceX();
+
+            for (int i = 0; i < list_paddles.Count; i++)
+            {
+                if (y < (list_paddles[i].y + list_paddles[i].height) && y > (list_paddles[i].y - list_paddles[i].height))
+                {
+                    if (Math.Abs(x - list_paddles[i].x) < (list_paddles[i].width + size + 1))
+                    {
+                        if (Math.Abs(x - list_paddles[i].x) > (list_paddles[i].width + size - 1))
+                        {
+                            bounceX();
+                            if (Math.Abs(list_paddles[i].yVel) > 0) yVel = list_paddles[i].yVel;
+                            list_paddles[i].calcOffset();
+                        }
+                        else
+                        {
+                            if (x > list_paddles[i].x) x += size * 2;
+                            else x -= size * 2;
+                        }
+                    }
+                }
+            }
+
+        }
+
+        public void draw()
+        {
+            Tyson.RenderBox(x, y, size, size, hue);
+        }
+
+        public void bounceX()
+        {
+            xVel = -xVel;
+        }
+        public void bounceY()
+        {
+            yVel = -yVel;
+        }
+    }
+
+    public class Paddle
+    {
+        public double x, y, yVel, width, height, speed, oldY, screenH, screenW, aiRNDoffset;
+        Color hue;
+        Key upKey, downKey;
+        bool AI = false;
+        int aiOffset;
+        Random rnd = new Random();
+
+        public Paddle(double _x, double _y, double _w, double _h, double _s, Color _c, Key _up, Key _down, double _sw, double _sh, bool _ai, int _aiOffset)
+        {
+            x = _x;
+            y = _y;
+            yVel = 0;
+            width = _w;
+            height = _h + 20;
+            speed = _s;
+            hue = _c;
+            upKey = _up;
+            downKey = _down;
+            screenH = _sh;
+            screenW = _sw;
+            AI = _ai;
+            aiOffset = _aiOffset;
+            calcOffset();
+        }
+
+        public void run(KeyboardState k, List<Ball> iBalls)
+        {
+            if (AI)
+            {
+                for (int i = 0; i < iBalls.Count; i++)
+                {
+                    double
+                        intercept = PONG_KIT.trig_f(x, iBalls[i]),
+                        offAbs = Math.Abs(aiRNDoffset),
+                        diff = Math.Abs(iBalls[i].x - x);
+
+                    if (diff < 1500 - offAbs && diff > 500 - offAbs)
+                    {
+                        setObjective(intercept + aiRNDoffset * 3);
+                    }
+                    else if (diff < 500 - offAbs)
+                    {
+                        setObjective(intercept);
+                    }
+                    else
+                    {
+                        setObjective(aiRNDoffset);
+                    }
+                }
+            }
+            else
+            {
+                if (k[upKey]) moveUp();
+                if (k[downKey]) moveDown();
+            }
+
+        }
+
+        public void draw(bool debug)
+        {
+            if (debug) Tyson.RenderBox(x, y, width, aiOffset * 2, Color.Green);
+            Tyson.RenderBox(x, y, width, height - 20, hue);
+            if (debug) Tyson.RenderBox(x, aiRNDoffset, 5, 5, Color.Orange);
+        }
+
+        void setObjective(double var)
+        {
+            if (y > var) moveDown();
+            if (y < var) moveUp();
+        }
+
+        public void calcVel()
+        {
+            yVel = y - oldY;
+            oldY = y;
+        }
+
+        public void calcOffset()
+        {
+            aiRNDoffset = new Random().Next(-aiOffset, aiOffset);
+        }
+
+        void moveUp()
+        {
+            if (y + height < screenH) y += speed;
+        }
+        void moveDown()
+        {
+            if (y - height > -screenH) y -= speed;
+        }
+
+    }
+
+}
---
+++ Pong/PongB.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using TysonTK;
+using OpenTK;
+using OpenTK.Input;
+
+namespace Pong
+{
+    public class PongB
+    {
+        public static void Main()
+        {
+
+            bool loop = true;
+            while (loop)
+            {
+                Console.Write("P#: ");
+                int input = 0;
+                string inputStr = Console.ReadLine();
+
+                try
+                {
+                    input = Int32.Parse(inputStr);
+
+                    if (input >= 0 && input <= 4)
+                    {
+
+                        PongB x = new PongB(input);
+                        Tyson.LAUNCH(x.Load, x.Logic, x.Render, CMD, 1, 1, false, Color.Black, "Pong", 60);
+                        loop = false;
+                    }
+
+                }
+                catch { }
+                Console.WriteLine("Invalid Input! Try again...");
+            }
+        }
+
+        static void CMD(string str)
+        {
+
+        }
+
+
+        // ---------------------
+
+
+        int numOfp = 2;
+
+        Key p1Up = Key.W, p1Down = Key.S,
+            p2Up = Key.Up, p2Down = Key.Down,
+            p3Up = Key.T, p3Down = Key.G,
+            p4Up = Key.KeypadSubtract, p4Down = Key.KeypadPlus;
+
+        double paddleXpos = 1000;
+
+        List<Paddle> paddleList = new List<Paddle>();
+        List<Ball> ballsList = new List<Ball>();
+        bool debug = false, pause = false, start = true, toggle = false;
+
+        public PongB(int num)
+        {
+            numOfp = num;
+        }
+
+        public void Load()
+        {
+            reset(numOfp);
+        }
+
+        public void Logic(MouseState mstat, KeyboardState kStat, double mx, double my)
+        {
+            if (start)
+            {
+                if (kStat[Key.Tab])
+                {
+                    if (!toggle)
+                    {
+                        if (pause) pause = false;
+                        else pause = true;
+                    }
+                    toggle = true;
+                }
+                else toggle = false;
+
+                if (!pause)
+                {
+                    for (int i = 0; i < paddleList.Count; i++)
+                    {
+                        paddleList[i].run(kStat, ballsList);
+                    }
+                    for (int i = 0; i < ballsList.Count; i++)
+                    {
+                        ballsList[i].run(paddleList);
+                    }
+
+                    for (int i = 0; i < paddleList.Count; i++)
+                    {
+                        paddleList[i].calcVel();
+                    }
+                }
+            }
+            else
+            {
+                if (kStat[Key.Space])
+                {
+                    start = true;
+                }
+            }
+        }
+
+        public void Render()
+        {
+            for (int i = 0; i < ballsList.Count; i++)
+            {
+                ballsList[i].draw();
+            }
+            for (int i = 0; i < paddleList.Count; i++)
+            {
+                paddleList[i].draw(debug);
+            }
+            if (pause) drawPauseSymbol(-PONG_KIT.WIN_W + 100, -PONG_KIT.WIN_H + 100, 50, PONG_KIT.STANDARD_COLOR);
+        }
+
+
+        // -------
+
+
+        Paddle makePaddle(double _x, double _y, int _aiVal, bool _ai, Color _c, Key _ku, Key _kd)
+        {
+            return new Paddle(_x, _y, 10, 100, 8, _c, _ku, _kd, PONG_KIT.WIN_W, PONG_KIT.WIN_H, _ai, _aiVal);
+        }
+        Paddle makePaddle(double _x, double _y, Color _c, Key _ku, Key _kd)
+        {
+            return makePaddle(_x, _y, 0, false, _c, _ku, _kd);
+        }
+        Paddle makePaddle(double _x, Color _c, Key _ku, Key _kd)
+        {
+            return makePaddle(_x, 0, 0, false, _c, _ku, _kd);
+        }
+        Paddle makePaddleAI(double _x, int aiVal, Color _c)
+        {
+            return makePaddle(_x, 0, aiVal, true, _c, Key.Z, Key.M);
+        }
+        void addBall(double x, double y, double xv, double yv)
+        {
+            ballsList.Add(new Ball(x, y, 10, 10, PONG_KIT.STANDARD_COLOR, PONG_KIT.WIN_W * 2, PONG_KIT.WIN_H, xv, yv));
+        }
+        int getRND(int v)
+        {
+            return new Random().Next(-v, v);
+        }
+
+        void reset(int playerNum)
+        {
+            paddleList.Clear();
+            ballsList.Clear();
+
+            switch (playerNum)
+            {
+                case 0:
+                    paddleList.Add(makePaddleAI(-paddleXpos, 100, PONG_KIT.APERTURE_BLUE));
+                    paddleList.Add(makePaddleAI(paddleXpos, 100, PONG_KIT.APERTURE_ORANGE));
+                    addBall(0, 0, -10, getRND(3));
+                    break;
+
+
+                case 1:
+                    paddleList.Add(makePaddleAI(-paddleXpos, 100, PONG_KIT.APERTURE_BLUE));
+                    paddleList.Add(makePaddle(paddleXpos, PONG_KIT.APERTURE_ORANGE, p2Up, p2Down));
+                    addBall(0, 0, 10, getRND(3));
+                    break;
+
+                case 2:
+                    paddleList.Add(makePaddle(-paddleXpos, PONG_KIT.APERTURE_BLUE, p1Up, p1Down));
+                    paddleList.Add(makePaddle(paddleXpos, PONG_KIT.APERTURE_ORANGE, p2Up, p2Down));
+                    addBall(0, 0, -10, getRND(3));
+                    break;
+                case 3:
+                    paddleList.Add(makePaddle(-paddleXpos, 200, PONG_KIT.APERTURE_PURPLE, p1Up, p1Down));
+                    paddleList.Add(makePaddle(-paddleXpos - 20, -200, PONG_KIT.APERTURE_CYAN, p3Up, p3Down));
+                    paddleList.Add(makePaddle(paddleXpos, PONG_KIT.APERTURE_ORANGE, p2Up, p2Down));
+                    addBall(-20, 20, -10, getRND(3));
+                    addBall(20, -20, 10, getRND(3));
+                    break;
+                case 4:
+                    paddleList.Add(makePaddle(-paddleXpos - 20, 200, PONG_KIT.APERTURE_CYAN, p1Up, p1Down));
+                    paddleList.Add(makePaddle(-paddleXpos, -200, PONG_KIT.APERTURE_PURPLE, p3Up, p3Down));
+                    paddleList.Add(makePaddle(paddleXpos, 200, PONG_KIT.APERTURE_RED, p2Up, p2Down));
+                    paddleList.Add(makePaddle(paddleXpos + 20, -200, PONG_KIT.APERTURE_YELLOW, p4Up, p4Down));
+                    addBall(-20, 20, -10, getRND(3));
+                    addBall(-20, -20, -10, -getRND(3));
+                    addBall(20, -20, 10, getRND(3));
+                    addBall(20, 20, 10, -getRND(3));
+
+                    break;
+            }
+        }
+
+        void drawPauseSymbol(double x, double y, double size, Color c)
+        {
+            double q = size / 4, j = size / 2;
+            Tyson.RenderBox(x - j, y, q, size, c);
+            Tyson.RenderBox(x + j, y, q, size, c);
+        }
+
+
+        
+    }
+
+   
+
+}
